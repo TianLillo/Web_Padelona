a266f98c924c6b8fca9e90c8bc6c2b53af6b0813
TianLillo <christian.lillo92@gmail.com>
2023-02-23 18:24:59 +0100

    primer up

---
+++ clubes.js
@@ -0,0 +1,67 @@
+var clubsLocation = [{
+		"id": "1",
+		"name": "Padel viu",
+		"latitude": "41.425268",
+		"longitude": "2.204492",
+	},
+	{
+		"id": "2",
+		"name": "Fes padel Can Drago",
+		"latitude": "41.437109",
+		"longitude": "2.184129",
+	},
+	{
+		"id": "3",
+		"name": "Padel 7 Glories",
+		"latitude": "41.399715",
+		"longitude": "2.193497",
+	},
+	{
+		"id": "4",
+		"name": "Padel Maresme",
+		"latitude": "41.412054",
+		"longitude": "2.213671",
+	},
+	{
+		"id": "5",
+		"name": "Espronceda Padel",
+		"latitude": "41.413285",
+		"longitude": "2.193939",
+	},
+	{
+		"id": "6",
+		"name": "Uno padel Meiland",
+		"latitude": "41.435113",
+		"longitude": "2.152235",
+	},
+	{
+		"id": "7",
+		"name": "Nick padel club",
+		"latitude": "41.435701",
+		"longitude": "2.154079",
+	},
+	{
+		"id": "8",
+		"name": "Padel 7 San Adreu",
+		"latitude": "41.426017",
+		"longitude": "2.197866",
+	},
+	{
+		"id": "9",
+		"name": "Club natacio Barcelona",
+		"latitude": "41.373608",
+		"longitude": "2.188728",
+	},
+	{
+		"id": "10",
+		"name": "Lets padel Mont Juic",
+		"latitude": "41.361982",
+		"longitude": "2.145192",
+	},
+	{
+		"id": "11",
+		"name": "Fair play padel club",
+		"latitude": "41.353286",
+		"longitude": "2.134812",
+	},
+	];
