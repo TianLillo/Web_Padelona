6651afaf2c0d717937e3c53fb6378d93692b5dbc
TianLillo <christian.lillo92@gmail.com>
2023-02-28 15:27:42 +0100

    add infowindow close

--- clubes.js
+++ clubes.js
@@ -3,6 +3,8 @@ var clubsLocation = [{
 		"name": "Padel viu",
 		"latitude": "41.425268",
 		"longitude": "2.204492",
+		"addres": "C/ de Binèfar, 15",
+		"phone": "+34 033 05 49 59",
 		"img": "img/pista1.png"
 	},
 	{
@@ -10,6 +12,8 @@ var clubsLocation = [{
 		"name": "Fes padel Can Drago",
 		"latitude": "41.437109",
 		"longitude": "2.184129",
+		"addres": "C/ de Rosselló i Porcel, 7",
+		"phone": "+34 626 73 80 45",
 		"img": "img/pista1.png"
 	},
 	{
@@ -17,6 +21,8 @@ var clubsLocation = [{
 		"name": "Padel 7 Glories",
 		"latitude": "41.399715",
 		"longitude": "2.193497",
+		"addres": "C/ dels Almogàvers, 169",
+		"phone": "+34 648 57 24 85",
 		"img": "img/pista1.png"
 	},
 	{
@@ -24,6 +30,8 @@ var clubsLocation = [{
 		"name": "Padel Maresme",
 		"latitude": "41.412054",
 		"longitude": "2.213671",
+		"addres": "C/ d'Antoni Gaudí, 22-28",
+		"phone": "+34 659 44 00 49",
 		"img": "img/pista1.png"
 	},
 	{
@@ -31,6 +39,8 @@ var clubsLocation = [{
 		"name": "Espronceda Padel",
 		"latitude": "41.413285",
 		"longitude": "2.193939",
+		"addres": "C. d'Espronceda, 281",
+		"phone": "+34 933 07 30 42",
 		"img": "img/pista1.png"
 	},
 	{
@@ -38,6 +48,8 @@ var clubsLocation = [{
 		"name": "Uno padel Meiland",
 		"latitude": "41.435113",
 		"longitude": "2.152235",
+		"addres": "Carrer de la Maternitat d'Elna, 1",
+		"phone": "+34 667 16 29 07",
 		"img": "img/pista1.png"
 	},
 	{
@@ -45,20 +57,26 @@ var clubsLocation = [{
 		"name": "Nick padel club",
 		"latitude": "41.435701",
 		"longitude": "2.154079",
+		"addres": "Carrer de Campoamor, 66",
+		"phone": "+34 605 67 13 55",
 		"img": "img/pista1.png"
 	},
 	{
 		"id": "8",
-		"name": "Padel 7 San Adreu",
+		"name": "Padel 7 Sant Adreu",
 		"latitude": "41.426017",
 		"longitude": "2.197866",
+		"addres": "Carrer de Bonaventura Gispert, 1",
+		"phone": "+34 627 94 45 48",
 		"img": "img/pista1.png"
 	},
 	{
 		"id": "9",
-		"name": "Club natacio Barcelona",
+		"name": "Club natacio Barceloneta",
 		"latitude": "41.373608",
 		"longitude": "2.188728",
+		"addres": "Plaça del Mar, s/n",
+		"phone": "+34 932 21 00 10",
 		"img": "img/pista1.png"
 	},
 	{
@@ -66,6 +84,8 @@ var clubsLocation = [{
 		"name": "Lets padel Mont Juic",
 		"latitude": "41.361982",
 		"longitude": "2.145192",
+		"addres": "Carrer del Segura, 36",
+		"phone": "+34 933 31 82 88",
 		"img": "img/pista1.png"
 	},
 	{
@@ -73,6 +93,8 @@ var clubsLocation = [{
 		"name": "Fair play padel club",
 		"latitude": "41.353286",
 		"longitude": "2.134812",
+		"addres": "Carrer del Foc, 2",
+		"phone": "+34 693 52 59 39",
 		"img": "img/pista1.png"
 	},
 	];
