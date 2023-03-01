fa777e5f0e5b25539b326dd37a27f1ece696631b
TianLillo <christian.lillo92@gmail.com>
2023-03-01 12:13:21 +0100

    todos los clubes

--- clubes.js
+++ clubes.js
@@ -5,7 +5,8 @@ var clubsLocation = [{
 		"longitude": "2.204492",
 		"addres": "C/ de Binèfar, 15",
 		"phone": "+34 033 05 49 59",
-		"img": "img/pista1.png"
+		"img": "img/pista1.png",
+		"web": "padelviu.html"
 	},
 	{
 		"id": "2",
@@ -14,7 +15,8 @@ var clubsLocation = [{
 		"longitude": "2.184129",
 		"addres": "C/ de Rosselló i Porcel, 7",
 		"phone": "+34 626 73 80 45",
-		"img": "img/pista1.png"
+		"img": "img/pista1.png",
+		"web": "fespadel.html"
 	},
 	{
 		"id": "3",
@@ -23,7 +25,8 @@ var clubsLocation = [{
 		"longitude": "2.193497",
 		"addres": "C/ dels Almogàvers, 169",
 		"phone": "+34 648 57 24 85",
-		"img": "img/pista1.png"
+		"img": "img/pista1.png",
+		"web": "padel7glories.html"
 	},
 	{
 		"id": "4",
@@ -32,7 +35,8 @@ var clubsLocation = [{
 		"longitude": "2.213671",
 		"addres": "C/ d'Antoni Gaudí, 22-28",
 		"phone": "+34 659 44 00 49",
-		"img": "img/pista1.png"
+		"img": "img/pista1.png",
+		"web": "padelmaresme.html"
 	},
 	{
 		"id": "5",
@@ -41,7 +45,8 @@ var clubsLocation = [{
 		"longitude": "2.193939",
 		"addres": "C. d'Espronceda, 281",
 		"phone": "+34 933 07 30 42",
-		"img": "img/pista1.png"
+		"img": "img/pista1.png",
+		"web": "espronceda.html"
 	},
 	{
 		"id": "6",
@@ -50,7 +55,8 @@ var clubsLocation = [{
 		"longitude": "2.152235",
 		"addres": "Carrer de la Maternitat d'Elna, 1",
 		"phone": "+34 667 16 29 07",
-		"img": "img/pista1.png"
+		"img": "img/pista1.png",
+		"web": "unopadel.html"
 	},
 	{
 		"id": "7",
@@ -59,7 +65,8 @@ var clubsLocation = [{
 		"longitude": "2.154079",
 		"addres": "Carrer de Campoamor, 66",
 		"phone": "+34 605 67 13 55",
-		"img": "img/pista1.png"
+		"img": "img/pista1.png",
+		"web": "nick.html"
 	},
 	{
 		"id": "8",
@@ -68,7 +75,8 @@ var clubsLocation = [{
 		"longitude": "2.197866",
 		"addres": "Carrer de Bonaventura Gispert, 1",
 		"phone": "+34 627 94 45 48",
-		"img": "img/pista1.png"
+		"img": "img/pista1.png",
+		"web": "padel7santadreu.html"
 	},
 	{
 		"id": "9",
@@ -77,7 +85,8 @@ var clubsLocation = [{
 		"longitude": "2.188728",
 		"addres": "Plaça del Mar, s/n",
 		"phone": "+34 932 21 00 10",
-		"img": "img/pista1.png"
+		"img": "img/pista1.png",
+		"web": "clubbarceloneta.html"
 	},
 	{
 		"id": "10",
@@ -86,7 +95,8 @@ var clubsLocation = [{
 		"longitude": "2.145192",
 		"addres": "Carrer del Segura, 36",
 		"phone": "+34 933 31 82 88",
-		"img": "img/pista1.png"
+		"img": "img/pista1.png",
+		"web": "letsmontjuic.html"
 	},
 	{
 		"id": "11",
@@ -95,6 +105,7 @@ var clubsLocation = [{
 		"longitude": "2.134812",
 		"addres": "Carrer del Foc, 2",
 		"phone": "+34 693 52 59 39",
-		"img": "img/pista1.png"
+		"img": "img/pista1.png",
+		"web": "fariplay.html"
 	},
 	];
